4b103f604746a9abf90cd59b96fe9ea84893fb5f
chirag r_s <chiragrs2003@gmail.com>
2023-07-04 17:38:30 +0530

    second

--- js/index.js
+++ js/index.js
@@ -1,15 +1,3 @@
-window.addEventListener("load", function () {
-  var preloader = document.getElementById("preloader");
-  preloader.style.opacity = 1;
-
-  setTimeout(function () {
-    preloader.style.opacity = 0;
-    setTimeout(function () {
-      preloader.style.display = "none";
-    }, 500); // Adjust the delay as needed
-  }, 2000); // Adjust the delay as needed
-});
-
 var currentImageIndex = 0;
 var images = document.querySelectorAll(".slider img");
 var marksContainer = document.querySelector(".slider__marks");
--- js/script.js
+++ js/script.js
@@ -27,14 +27,3 @@ var swiper = new Swiper(".slide-content", {
     },
   },
 });
-window.addEventListener("load", function () {
-  var preloader = document.getElementById("preloader");
-  preloader.style.opacity = 1;
-
-  setTimeout(function () {
-    preloader.style.opacity = 0;
-    setTimeout(function () {
-      preloader.style.display = "none";
-    }, 500); // Adjust the delay as needed
-  }, 2000); // Adjust the delay as needed
-});
